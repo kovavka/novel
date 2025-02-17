5fc00cccc82967b90aa40fee70efff053d58714b
Gin Kapger <gikapger@gmail.com>
2025-02-18 02:16:24 +0500

    Add promise to image load

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
 import { Scene } from './components/scene.tsx'
 import { preloadImages } from './images.ts'
-
-preloadImages()
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    preloadImages().then(() => setIsLoaded(true))
+  }, [])
+
+  if (!isLoaded) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className='app'>
       <Scene sceneId='0' />
--- site/src/images.ts
+++ site/src/images.ts
@@ -3,21 +3,27 @@ const images = import.meta.glob('/src/assets/**/*.png', { eager: true })
 export const backgrounds: Record<string, string> = {}
 export const characters: Record<string, string> = {}
 
-export const preloadImages = () => {
-  Object.values(images).forEach(module => {
-    const img = new Image()
-    const src = (module as { default: string }).default
-    img.src = src
+export const preloadImages = async (): Promise<void> => {
+  const promises = Object.values(images).map(module => {
+    return new Promise<void>(resolve => {
+      const img = new Image()
+      const src = (module as { default: string }).default
+      img.src = src
+      img.onload = () => resolve()
+      img.onerror = () => resolve() // Prevent blocking due to errors
 
-    const [folder, fileNameFull] = src.split('/').slice(-2)
-    const fileName = fileNameFull.includes('-')
-      ? fileNameFull.split('-')[0]
-      : fileNameFull.split('.')[0]
+      const [folder, fileNameFull] = src.split('/').slice(-2)
+      const fileName = fileNameFull.includes('-')
+        ? fileNameFull.split('-')[0]
+        : fileNameFull.split('.')[0]
 
-    if (folder === 'backgrounds') {
-      backgrounds[fileName] = src
-    } else if (folder === 'characters') {
-      characters[fileName] = src
-    }
+      if (folder === 'backgrounds') {
+        backgrounds[fileName] = src
+      } else if (folder === 'characters') {
+        characters[fileName] = src
+      }
+    })
   })
+
+  await Promise.all(promises)
 }
